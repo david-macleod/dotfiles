39a5db523cf9ec38a098ce6b36aae54ec02323c3
David MacLeod <macleod.david@live.co.uk>
2019-07-25 08:24:04 +0100

    added anaconda/miniconda to path

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,6 +1,9 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
+# Setup anaconda
+export PATH=$HOME/anaconda3/bin:$HOME/miniconda3/bin:$PATH
+
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
