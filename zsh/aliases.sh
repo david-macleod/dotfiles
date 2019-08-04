1645d3f53213c32f8a51d0ee48bed199d326a6d0
David MacLeod <macleod.david@live.co.uk>
2019-08-04 12:19:52 +0100

    added safe rm

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -3,6 +3,8 @@ alias ns="nvidia-smi"
 alias cuda0="export CUDA_VISIBLE_DEVICES=0"
 alias cuda1="export CUDA_VISIBLE_DEVICES=1"
 
+alias rm="rm -i"
+
 # git plugin overrides
 alias gcm="git commit -m"
 alias gcom="git checkout master"
