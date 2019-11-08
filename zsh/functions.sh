f28b7bec9f6e3a97c4f8a741f9039fc76bd13a89
David MacLeod <davidma@speechmatics.com>
2019-11-08 14:21:07 +0000

    added qless

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -21,6 +21,14 @@ qtail () {
   if [ "$#" -eq 1 ]; then
     tail -f $(qstat -j $1 | grep log | grep std | cut -d ":" -f4)
   else
-    echo "Usage: qcat <jobid>" >&2
+    echo "Usage: qless <jobid>" >&2
+  fi
+}
+
+qless () {
+  if [ "$#" -eq 1 ]; then
+    less $(qstat -j $1 | grep log | grep std | cut -d ":" -f4)
+  else
+    echo "Usage: qless <jobid>" >&2
   fi
 }
