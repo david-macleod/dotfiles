ea44e6d146de2a16f9df49bc78e1c674b61d0744
David MacLeod <davidma@speechmatics.com>
2019-11-27 22:03:17 +0000

    added tb

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -38,3 +38,48 @@ qdesc () {
     echo "$job $(qstat -j $job | grep log | grep std | rev | cut -d "/" -f2 | rev)"
   done
 }
+
+tblink () {
+  if [ "$#" -eq 0 ]; then
+    logdir=$(pwd)
+  else
+  # setup tensorboard directory
+    tbdir="$HOME/tb"
+    if [ -d "$tbdir" ]; then
+      
+      last=$(ls -v $tbdir | tail -1)
+      new=$((last+1))
+      logdir="$tbdir/$new"
+    else
+      logdir="$tbdir/0"
+    fi
+    mkdir -p $logdir
+
+    # softlink into tensorboard directory
+    for linkdir in "$@"; do
+      linkdir=$(rl $linkdir)
+      echo "linkdir: $linkdir"
+      ln -s $linkdir $logdir
+    done
+  fi
+
+  echo "logdir: $logdir"
+
+  $HOME/venvs/tensorboard/bin/tensorboard --host=$(hostname) --logdir=$logdir
+}
+  
+tb () {
+
+  session=tb
+  # create tb tmux session if not existing
+  tmux has-session -t $session 2>/dev/null
+  if [ $? != 0 ]; then
+    tmux new -d -s $session
+  else
+    tmux split-window -v -p 90 -t $session
+  fi
+
+  # run tensorboard
+  tmux send -t $session "tblink $@" ENTER
+  sleep 3 && tmux capture-pane; tmux show-buffer | grep -v -e '^$'
+}
