57906b536a6e97633357f89e2de633becf5f4323
David MacLeod <davidma@speechmatics.com>
2020-05-08 19:39:03 +0000

    added ipdb debugging

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-echo "source $HOME/dotfiles/zsh/zshrc.sh" > $HOME/.zshrc
-echo "source $HOME/dotfiles/tmux/tmux.conf" > $HOME/.tmux.conf
+echo "source $HOME/dotfiles/zsh/.zshrc.sh" > $HOME/.zshrc
+echo "source $HOME/dotfiles/tmux/.tmux.conf" > $HOME/.tmux.conf
+cp -f $HOME/dotfiles/.pdbrc $HOME/.pdbrc
 zsh
