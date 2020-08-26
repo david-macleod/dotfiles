06ee56340cb09af54f34500f652706a60cbeb655
David MacLeod <davidma@speechmatics.com>
2020-08-26 08:48:50 +0000

    added q and wq

--- zsh/aliases-sm.sh
+++ zsh/aliases-sm.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
-alias getgpu="qlogin -q gpu.q"
-alias get980="qlogin -q gpu.q@@980"
-alias gettitanx="qlogin -q gpu.q@@titanx"
 alias qq="qstat -f -u '*' | less "
+alias q="qstat -f -u '*' -q 'gpu.q*' | head -n40 | grep -v '\-\-\-\-' | grep -v queuename | grep -v '######' | grep -v '\- PENDING'"
+alias wq="watch 'qstat -f -u '\''*'\'' -q '\''gpu.q*'\'' | head -n40 | grep -v '\''\-\-\-\-'\'' | grep -v queuename | grep -v '\''######'\'' | grep -v '\''\- PENDING'\'"
 alias nsp="ps -up $(nvidia-smi -q -x | grep pid | sed -e 's/<pid>//g' -e 's/<\/pid>//g' -e 's/^[[:space:]]*//')"
